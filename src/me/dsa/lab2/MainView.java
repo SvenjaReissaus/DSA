d7a88b4d5f2bfd05b206fc1c32ed300d599265f1
rgrio <griosbozar@gmail.com>
2022-11-15 00:38:54 -0600

    Starting the lab2, creating the views for sorting and searching

--- src/me/dsa/lab2/MainView.java
+++ src/me/dsa/lab2/MainView.java
@@ -1,4 +1,17 @@
 package me.dsa.lab2;
 
-public class MainView {
+import me.dsa.AbstractMainView;
+import javax.swing.*;
+
+public class MainView extends AbstractMainView {
+    private JPanel content;
+    private JButton ordenarButton;
+    private JButton buscarButton;
+    private JButton atrasButton;
+
+    public MainView() {
+        super();
+        setContentPane(content);
+        pack();
+    }
 }
---
+++ src/me/dsa/lab2/SearchView.java
@@ -0,0 +1,7 @@
+package me.dsa.lab2;
+
+import javax.swing.*;
+
+public class SearchView {
+    private JPanel content;
+}
